568ed24272af664a9a38439acb5111a465b4ab46
renanpmiranda <renanpmiranda5@gmail.com>
2023-02-07 11:08:48 -0300

    criação das pastas router e errors com refatoração dos endpoints create

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { UserBusiness } from "../business/UserBusiness"
+import { BaseError } from "../errors/BaseError"
 
 export class UserController {
     public getUsers = async (req: Request, res: Response) => {
@@ -11,14 +12,10 @@ export class UserController {
     
             res.status(200).send(output)
         } catch (error) {
-            console.log(error)
+            console.log(error)            
     
-            if (req.statusCode === 200) {
-                res.status(500)
-            }
-    
-            if (error instanceof Error) {
-                res.send(error.message)
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message)
             } else {
                 res.send("Erro inesperado")
             }
